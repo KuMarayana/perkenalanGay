a3877fa760b340a1d91f94a4f1cea6c1302e0ae4
KuMarayana <nengahrupini16@gmail.com>
2021-06-01 05:51:24 -0400

    javax

---
+++ main.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+
+public class UseOtionsPanes {
+
+    public static void main(String [] args) {
+
+       String name = JOptionPane.showInputDialog(null,
+            "siapa kah namamu?");
+
+      //menanyakan yes dan tidak
+       int choice = JOptionPane.showConfirmDialog(null,
+            "what do you gay, + name + ?");
+
+       if (choice == JOptionPane.YES_OPTION) {
+       JOptionPane.showMessageDialog(null,
+            "tentu saja kamu gay?");
+
+      } else { // choice == NO_OPTION or CANCEL_OPTION
+           JOptionPane.showMessageDialog(null,
+            "apakah anda setuju bahwa anda gay?");
+  
+      }
+
+   }
+
+}
